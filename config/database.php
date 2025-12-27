213643770378d13fd4ff90dae103bae315722dca
dhimxanity <dhimasarya37@gmail.com>
2025-12-27 14:10:11 +0800

    Mengisi codingan pada bagian database.php

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+// Menyimpan konfigurasi database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "todo_db";
+
+// Membuat koneksi ke database
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Mengecek apakah koneksi berhasil
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
